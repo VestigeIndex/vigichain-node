4cfa57e69305e381e15564c490a18620fc2563a2
Utxo Labs Team <inf.vestigeindex@gmail.com>
2026-09-10 18:40:38 +0200

    style: load Mission Control dedicated view surfaces

--- apps/vigi-miner/src/main.tsx
+++ apps/vigi-miner/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './extras.css';
 import './compact.css';
+import './views.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
